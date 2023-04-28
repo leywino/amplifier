4225e0c1d5a0b2f3d20fe5fd10b28cfbf1739c36
leywino <muhammadarshadv2@gmail.com>
2023-04-28 20:13:43 +0530

    home screen ui completed

---
+++ lib/core/icons/genereal_icons.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+SvgPicture cHeartIcon = SvgPicture.asset(
+  "assets/icons/heart.svg",
+  color: Colors.red,
+);
