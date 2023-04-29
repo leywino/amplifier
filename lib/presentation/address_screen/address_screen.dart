fb4d7ab88b66f8cb9ab566c195c838549048a4cf
leywino <muhammadarshadv2@gmail.com>
2023-04-29 18:38:24 +0530

    address ui completed

--- lib/presentation/address_screen/address_screen.dart
+++ lib/presentation/address_screen/address_screen.dart
@@ -1,50 +1,142 @@
+import 'package:amplifier/core/colors/main_colors.dart';
 import 'package:amplifier/presentation/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class AddressScreen extends StatelessWidget {
-  const AddressScreen({super.key});
+  AddressScreen({super.key});
+
+  ValueNotifier<int> radioNotifier = ValueNotifier(0);
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
+      backgroundColor: kMainBgColor,
       body: Column(children: [
         CustomAppBar(title: "Address", showBackButton: true),
         Column(
           children: List.generate(
-            1,
-            (index) => ListTile(
-              leading: CircleAvatar(
-                child: Icon(Icons.person),
-              ),
-              title: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'John Doe',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+            3,
+            (index) => Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 6),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(
+                      12.0), // set the border radius to 12.0
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Colors.grey[400],
+                      child: SvgPicture.asset("assets/icons/location.svg"),
+                    ),
+                    title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'John Doe',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 4.0),
+                        FittedBox(
+                          child: Text(
+                            'johndoe@example.com',
+                            style: TextStyle(
+                                color: Colors.grey[600], fontSize: 12),
+                          ),
+                        ),
+                      ],
+                    ),
+                    trailing: ValueListenableBuilder(
+                      valueListenable: radioNotifier,
+                      builder: (context, radioIndex, child) => Radio(
+                        value: radioIndex == index,
+                        groupValue: true,
+                        onChanged: (value) {
+                          radioNotifier.value = index;
+                        },
+                        activeColor: Colors.black,
+                      ),
                     ),
                   ),
-                  SizedBox(height: 4.0),
-                  Text(
-                    'johndoe@example.com',
-                    style: TextStyle(
-                      color: Colors.grey[500],
-                    ),
-                  ),
-                ],
-              ),
-              trailing: Radio(
-                value: true,
-                groupValue: true,
-                onChanged: (value) {},
-                activeColor: Colors.black,
+                ),
               ),
             ),
           ),
-        )
+        ),
+        SizedBox(
+          height: size.height * 0.05,
+        ),
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              TextButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      Colors.grey.withOpacity(0.4)),
+                  padding: MaterialStateProperty.all<EdgeInsets>(
+                    EdgeInsets.symmetric(
+                        horizontal: size.width * 0.195, vertical: 20),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SvgPicture.asset("assets/icons/location.svg"),
+                    Text(
+                      'Add New Address',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      side: BorderSide(color: Colors.black),
+                    ),
+                  ),
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
+                  padding: MaterialStateProperty.all<EdgeInsets>(
+                    EdgeInsets.symmetric(
+                        horizontal: size.width * 0.38, vertical: 20),
+                  ),
+                ),
+                child: Text(
+                  'Apply',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: size.height * 0.03,
+        ),
       ]),
     );
   }
